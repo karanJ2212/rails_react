5c1a8f72b98896820d05fae53cb57c3cf539a3f8
karan jain <karanjain2212@gmail.com>
2023-12-08 17:20:32 +0530

    implemented delete

--- client/src/feature/posts/PostsList.jsx
+++ client/src/feature/posts/PostsList.jsx
@@ -29,16 +29,35 @@ export default function PostsList() {
     }
     loadPost();
   }, []);
+
+  const deletePosts = async (id) => {
+    try {
+      const res = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        setPost(posts.filter((post) => post.id !== id));
+      } else {
+        throw res;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <h1>Post List</h1>
       {posts.map((post) => (
         <div key={post.id} className="post-container">
-          <h2>{post.title}</h2>
-          <Link to={`posts/${post.id}`} className="post-title">
-            {post.title}
-          </Link>
-          <p>{post.body}</p>
+          <h2>
+            {/* {post.title} */}
+            <Link to={`posts/${post.id}`} className="post-title">
+              {post.title}
+            </Link>
+          </h2>
+          <div className="Post-links">
+            <button onClick={() => deletePosts(post.id)}>Delete</button>
+          </div>
         </div>
       ))}
     </>
